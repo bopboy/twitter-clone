1302caada7f05882e028a4d0f9f52cfac7de9662
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-03 06:35:24 +0900

    12 계정 생성 (로그인 폼 활용)
    fb 9 버전 사용 (강의와 다름)

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react"
+import { authService } from "fbase"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 
 const Auth = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [newAccount, setNewAccount] = useState(true)
     const onChange = (e) => {
         const { target: { name, value } } = e
         if (name === "email") setEmail(value)
         else if (name === "password") setPassword(value)
     }
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let data
+            if (newAccount) {
+                data = await createUserWithEmailAndPassword(authService, email, password)
+            } else {
+                data = await signInWithEmailAndPassword(authService, email, password)
+            }
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-                <input type="submit" value="로그인" />
+                <input type="submit" value={newAccount ? "계성 생성" : "로그인"} />
             </form>
             <div>
                 <button>Login with Google</button>
