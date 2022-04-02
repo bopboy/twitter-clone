77eb845bd39556a1f6e7492579d1421dd4e744d9
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-03 08:15:32 +0900

    18 트윗 조회 출력

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { dbInstance } from "fbase"
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc, getDocs } from 'firebase/firestore'
 
 const Home = () => {
     const [tweet, setTweet] = useState("")
+    const [tweets, setTweets] = useState([])
+    const getTweets = async () => {
+        const tweetsFromDB = await getDocs(collection(dbInstance, "tweets"))
+        tweetsFromDB.forEach(doc => {
+            const tweetObj = { ...doc.data(), id: doc.id }
+            setTweets(prev => [tweetObj, ...prev])
+        })
+    }
+    useEffect(() => {
+        getTweets()
+    }, [])
     const onChange = (e) => {
         const { target: { value } } = e
         setTweet(value)
@@ -15,11 +26,12 @@ const Home = () => {
     }
     return (
         <div>
+            {tweets.map(t => (<div key={t.id}><h4>{t.tweet}</h4></div>))}
             <form onSubmit={onSubmit}>
-                <input type="text" placeholder="what's on your mind?" maxLength={120} onChange={onChange} />
-                <input type="submit" value="Tweet" />
+                <input type="text" placeholder="what's on your mind?" maxLength={120} onChange={onChange} value={tweet} />
+                <input type="submit" value="tweet" />
             </form>
-        </div>
+        </div >
     )
 }
 export default Home
