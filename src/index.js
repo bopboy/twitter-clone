31f135d0b63bd3e0dd7995492a18a8537b6812c2
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-02 20:44:55 +0900

    07 프로젝트 설정, 파이어베이스 설정, .env 추가
    리액트 18 버전 경고 떠서 17 버전으로 재설치

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import app from './firebase';
+console.log(app)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
