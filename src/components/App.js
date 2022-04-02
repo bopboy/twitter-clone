a1c80115b799000ba1a4ef71c0530a101336f24b
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-03 06:45:58 +0900

    13 이메일 로그인 기능

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from 'fbase'
 
 function App() {
+  const [init, setInit] = useState(false)
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  useEffect(() => {
+    authService.onAuthStateChanged(user => {
+      if (user) setIsLoggedIn(true)
+      else setIsLoggedIn(false)
+      setInit(true)
+    })
+  }, [])
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "초기화 중..."}
       <footer>&copy; {new Date().getFullYear()} Twitter-Clone</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,6 +6,7 @@ const Auth = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [newAccount, setNewAccount] = useState(true)
+    const [error, setError] = useState("")
     const onChange = (e) => {
         const { target: { name, value } } = e
         if (name === "email") setEmail(value)
@@ -22,16 +23,19 @@ const Auth = () => {
             }
             console.log(data)
         } catch (error) {
-            console.log(error)
+            setError(error.message)
         }
     }
+    const toggleAccount = () => setNewAccount(prev => !prev)
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
                 <input type="submit" value={newAccount ? "계성 생성" : "로그인"} />
+                {error}
             </form>
+            <span onClick={toggleAccount}>{newAccount ? "로그인" : "계정 생성"}</span>
             <div>
                 <button>Login with Google</button>
                 <button>Login with Github</button>
