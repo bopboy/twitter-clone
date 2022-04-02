6719e44d89e4e5993ae7fdb8667b58f0eb7a37a2
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-03 05:59:36 +0900

    09 fb Auth 사용 확인
    jsconfig.json 추가 (vscode 재시작 필요)

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Auth from "../routes/Auth"
 import Home from "../routes/Home"
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
+const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
             <Switch>
