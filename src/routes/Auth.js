057128422daea85fc491953cb89df382a4a79473
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-03 06:14:34 +0900

    10 fb auth 설정, 로그인 폼 작성
    fb auth 는 이메일과 구글, 깃허브로

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,2 +1,26 @@
-import React from "react"
-export default () => <span>Auth</span>
+import React, { useState } from "react"
+
+const Auth = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const onChange = (e) => {
+        const { target: { name, value } } = e
+        if (name === "email") setEmail(value)
+        else if (name === "password") setPassword(value)
+    }
+    return (
+        <div>
+            <form>
+                <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+                <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
+                <input type="submit" value="로그인" />
+            </form>
+            <div>
+                <button>Login with Google</button>
+                <button>Login with Github</button>
+            </div>
+        </div>
+    )
+}
+
+export default Auth
